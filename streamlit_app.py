02e1a47eda394aad9308309385c24d63745c515c
almeidava93 <almeida.va93@gmail.com>
2023-05-26 19:31:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,20 +28,21 @@ import importlib
 def install_and_import():
     try:
         # Try importing the module
-        importlib.import_module("med_code_search.main")
+        importlib.import_module("med_code_search")
     except ImportError:
         # Install the module if not already installed
         subprocess.check_call([sys.executable, "-m", "pip", "install", f"git+https://{st.secrets['github_token']}@github.com/almeidava93/med_code_search.git"])
     finally:
         # If the module is installed, import it
-        if importlib.util.find_spec("med_code_search.main") is not None:
-            globals()["med_code_search.main"] = importlib.import_module("med_code_search.main")
+        if importlib.util.find_spec("med_code_search") is not None:
+            globals()["med_code_search"] = importlib.import_module("med_code_search")
         else:
-            print(f"Failed to install and import med_code_search.main")
+            print(f"Failed to install and import med_code_search")
 
 install_and_import()
 
 # Here comes the code that will only run if numpy is installed.
-if 'med_code_search.main' in globals():
+if 'med_code_search' in globals():
     import med_code_search.main as main
+
     main.app()
