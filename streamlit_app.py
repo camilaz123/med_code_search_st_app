d5b430b02e92034e5e8edcb515875ec7efd2e3b8
almeidava93 <almeida.va93@gmail.com>
2023-09-10 16:05:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,14 @@ def install_and_import():
         # If the module is installed, import it
         if importlib.util.find_spec("med_code_search") is not None:
             globals()["med_code_search"] = importlib.import_module("med_code_search")
+            import med_code_search.main as main
+            main.app()
         else:
             print(f"Failed to install and import med_code_search")
 
 install_and_import()
 
-# # Here comes the code that will only run if numpy is installed.
-# if 'med_code_search' in globals():
-import med_code_search.main as main
+# # # Here comes the code that will only run if numpy is installed.
+# # if 'med_code_search' in globals():
+# import med_code_search.main as main
 
-main.app()
