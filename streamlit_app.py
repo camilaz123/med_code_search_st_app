eda48657c92823ccfbfbee66ab885177590f110c
almeidava93 <almeida.va93@gmail.com>
2023-09-10 15:53:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,25 @@
-# import subprocess
-# import sys
-# import streamlit as st
-# import importlib
-import med_code_search.main as main
+import subprocess
+import streamlit as st
+import importlib
 
-main.app()
+def install_and_import():
+    try:
+        # Try importing the module
+        importlib.import_module("med_code_search")
+    except ImportError:
+        # Install the module if not already installed
+        subprocess.check_call(["python3", "-m", "pip", "install", "--user", f"git+https://almeidava93:{st.secrets['github_token']}@github.com/almeidava93/med_code_search.git"])
+    finally:
+        # If the module is installed, import it
+        if importlib.util.find_spec("med_code_search") is not None:
+            globals()["med_code_search"] = importlib.import_module("med_code_search")
+        else:
+            print(f"Failed to install and import med_code_search")
 
-# def install_and_import():
-#     try:
-#         # Try importing the module
-#         importlib.import_module("med_code_search")
-#     except ImportError:
-#         # Install the module if not already installed
-#         subprocess.check_call(["python3", "-m", "pip", "install", "--user", f"git+https://{st.secrets['github_token']}@github.com/almeidava93/med_code_search.git"])
-#     finally:
-#         # If the module is installed, import it
-#         if importlib.util.find_spec("med_code_search") is not None:
-#             globals()["med_code_search"] = importlib.import_module("med_code_search")
-#         else:
-#             print(f"Failed to install and import med_code_search")
+install_and_import()
 
-# install_and_import()
+# Here comes the code that will only run if numpy is installed.
+if 'med_code_search' in globals():
+    import med_code_search.main as main
 
-# # Here comes the code that will only run if numpy is installed.
-# if 'med_code_search' in globals():
-#     import med_code_search.main as main
-
-#     main.app()
+    main.app()
