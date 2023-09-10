94fccc1a95050151bf24811858d3844978f17eba
almeidava93 <almeida.va93@gmail.com>
2023-09-10 16:31:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,13 @@ def install_and_import():
     finally:
         # If the module is installed, import it
         if importlib.util.find_spec("med_code_search") is not None:
-            globals()["med_code_search"] = importlib.import_module("med_code_search")
+            import med_code_search.main as main
         else:
             print(f"Failed to install and import med_code_search")
 
 install_and_import()
 
-if 'med_code_search' in globals():
+if importlib.util.find_spec("med_code_search") is not None:
     import med_code_search.main as main
 
     main.app()
